815555bf14e14634b7925394068349296825092c
trenton-slocum <trent.slocum@hotmail.com>
2024-07-02 11:17:06 -0400

    Finished package

---
+++ package/__init__.py
@@ -0,0 +1,1 @@
+print('Welcome to my module. I have a story for you')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'The Story of Darth Plagueis the Wise',
+    version = '1.0',
+    description = 'A story for the ages',
+    url = 'https://github.com/trenton-slocum/DS5100-M09-Challenge',
+    author = 'T.S. Slocum',
+    license = 'MIT',
+    packages = ['package']
+)
